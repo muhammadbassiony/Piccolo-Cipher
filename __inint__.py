1b45b05e44387de8a5375ebcf74395c4eec945dd
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-06-16 19:37:17 +0200

    finished all round key gens

--- __inint__.py
+++ __inint__.py
@@ -1,9 +1,10 @@
 from piccolo import Piccolo
 
 bit = 80
-key = 0x1169207e11f0ffee1476
+key80 = 0x1169207e11f0ffee1476
+key128 = 0x433c6ab617af29a3c8163fad92c3d834
 
-p = Piccolo(key=key, bit=80)
+p = Piccolo(key=key80, bit=bit)
 
 cipher = p.encrypt("hellomax")
 # print(cipher)
--- piccolokeyscheduling.py
+++ piccolokeyscheduling.py
@@ -10,7 +10,7 @@ class InvalidValue(Exception):
 def split_bits(value, n):
 
     mask, parts = (1 << n) - 1, []
-    print('MASK :: ', hex(mask), bin(mask))
+    # print('MASK :: ', hex(mask), bin(mask))
     parts = []
     while value:
         parts.append(value & mask)
@@ -29,7 +29,7 @@ def generate_white_keys(bit, key):
     k = []
     wk = []
 
-    # print('ENTERING SPLITTER :: ', bit, key)
+    # print('ENTERING SPLITTER :: ', bit, hex(key))
     k = split_bits(key, 16)
     # print('KEY SPLIT ::: ', [hex(x) for x in k])
 
@@ -54,7 +54,7 @@ def generate_white_keys(bit, key):
 
 
 def generate_round_keys(bit, key):
-    print('GEN ROUND KEYS :: PAR4AMS ::: ', bit, key)
+    # print('GEN ROUND KEYS :: PAR4AMS ::: ', bit, hex(key))
     ikey = int(key)
     skey = str(key)
     k = []
@@ -62,62 +62,73 @@ def generate_round_keys(bit, key):
     sbit = int(bit / 16)
 
     k = split_bits(key, 16)
-    print('KEY SPLIT ::: ', [hex(x) for x in k])
+    # print('KEY SPLIT IN RK ::: ', [hex(x) for x in k])
 
     if bit == 80:
         r = 25
     elif bit == 128:
         r = 31
 
-    rk = np.ones((2*r + 1), dtype=int)
-    cons = np.ones((2*r + 1), dtype=int)
-    print(rk.shape, cons.shape, type(cons), print(cons[30]))
+
+    # rk = np.zeros((2*r + 2), dtype=int)
+    # cons = np.zeros((2*r + 2), dtype=int)
+    # print(rk.shape, cons.shape, type(cons), (2*r + 1), 2*r-1)
+    # print('ZERO CHECK :: ', [hex(x) for x in cons])
 
 
     if bit == 80:
+
+        rk = np.zeros((2 * r ), dtype=int)
+        cons = np.zeros((2 * r ), dtype=int)
+        # print('RK80 SHAPES :: ',rk.shape, cons.shape, type(cons), (2 * r + 1), 2 * r - 1)
+
         for i in range(r-1):
             #generate constant
-            l, r = get_contsant_values(i, bit)
-            cons[2*i] = l
-            cons[2*i + 1] = r
-            # print('CON80 RK :: ', i, hex(l), hex(r), hex(cons[2*i]), hex(cons[2*i + 1]))
+            left, right = get_contsant_values(i, bit)
+            cons[2*i] = left
+            cons[(2*i)+1] = right
+            # print('CON80 RK :: ', i, hex(l), hex(r), hex(cons[2*i]), hex(cons[(2*i)+1]))
 
             if i % 5 == 2 or i % 5 == 0:
                 z = cons[2*i] ^ k[2]
                 rk[2*i] = z
 
-                z = cons[2*i + 1] ^ k[3]
+                z = cons[(2*i)+1] ^ k[3]
                 rk[2*i + 1] = z
 
             elif i % 5 == 1 or i % 5 == 4:
                 z = cons[2 * i] ^ k[0]
                 rk[2 * i] = z
 
-                z = cons[2 * i + 1] ^ k[1]
+                z = cons[(2*i)+1] ^ k[1]
                 rk[2 * i + 1] = z
 
             elif i % 5 == 3:
                 z = cons[2 * i] ^ k[4]
                 rk[2 * i] = z
 
-                z = cons[2 * i + 1] ^ k[4]
+                z = cons[(2*i)+1] ^ k[4]
                 rk[2 * i + 1] = z
 
-        print('DONE 80 RK GEN :: ')
+        # print('DONE 80 RK GEN :: ')
 
     elif bit == 128:
-        for i in range(2*r - 1):
 
+        rk = np.zeros((2 * r + 1), dtype=int)
+        cons = np.zeros(2 * (2 * r + 1), dtype=int)
+        # print('RK128 SHAPES :: ', rk.shape, cons.shape, type(cons), (2 * r + 1), 2 * r - 1)
+
+        for i in range((2*r) - 1):
             # generate constant
-            con = _constant_value_128(i)
-            # split con
-            con2i_1 = con[16:32]
-            con2i = con[0:16]
-            con2i_1 = hex(int(con2i_1, 2))
-            con2i = hex(int(con2i, 2))
+            left, right = get_contsant_values(i, bit)
+            cons[2 * i] = left
+            cons[(2*i)+1] = right
+            # print('\nCON128 RK :: ', i, hex(left), hex(right), hex(cons[2*i]), hex(cons[(2*i)+1]))
 
 
+            # print('KEY BEFORE :: ', [hex(x) for x in k])
             if (i + 2) % 8 == 0:
+                # print('SWITCHEROO', [hex(x) for x in k])
                 tmp = k
                 k[0] = tmp[2]
                 k[2] = tmp[6]
@@ -126,62 +137,29 @@ def generate_round_keys(bit, key):
                 k[5] = tmp[3]
                 k[6] = tmp[4]
                 k[7] = tmp[5]
-
+                # print('KEY AFTER :: ', [hex(x) for x in k])
 
             indx = (i + 1) % 8
-
-            a = int(k[indx], 16)
-            b = int(con2i[2:], 16)
-
-            x = a ^ b
-            z = bin(x)[2:].zfill(16)
-
+            z = k[indx] ^ cons[i]
             rk[i] = z
 
     else:
         raise InvalidValue('bit=' + str(bit), 'The value of bit can be 80 or 128')
 
-    # print('ROUND KEYS GENNED :: ', rk)
+    print('ROUND KEYS GENNED :: ', [hex(x) for x in rk])
     return rk
 
+
+
 def get_contsant_values(i, bit):
+
     cnn = 0x0f1e2d3c if bit==80 else 0x6547a98b
     ci_1 = (i + 1)
     cc = (ci_1 << 27) | (0 << 22) | (ci_1 << 17) | (0 << 15) | (ci_1 << 10) | (0 << 5) | (ci_1)
+
     cons = cc ^ cnn
     left = cons >> 16
     right = cons & 0xffff
 
     return (left, right)
 
-
-def _constant_value_80(i):
-        cnn = "0f1e2d3c"
-        ci1 = str(bin(i+1)[2:].zfill(5))
-        c0 = str(bin(0)[2:].zfill(5))
-        #convert to binary string
-        c = ci1 + c0 + ci1 + "00" + ci1 + c0 + ci1
-        c_hex = hex(int(c, 2))
-
-        a = int(c_hex[2:], 16)
-        b = int(cnn, 16)
-        x = a^b
-        con = bin(x)[2:].zfill(32)
-        # print('HERE :: ', con, len(con))
-        return con
-
-
-def _constant_value_128(i):
-    cnn = "6547a98b"
-    ci1 = str(bin(i + 1)[2:].zfill(5))
-    c0 = str(bin(0)[2:].zfill(5))
-    # convert to binary string
-    c = ci1 + c0 + ci1 + "00" + ci1 + c0 + ci1
-    c_hex = hex(int(c, 2))
-
-    a = int(c_hex[2:], 16)
-    b = int(cnn, 16)
-    x = a ^ b
-    con = bin(x)[2:].zfill(32)
-    # print('HERE :: ', c, con, len(con))
-    return con
